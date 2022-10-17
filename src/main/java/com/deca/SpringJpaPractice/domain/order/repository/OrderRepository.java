15e43d9d68dfbf19d33b07c67e4dda7d1a48dd06
JeongHunHui <deca3248@gmail.com>
2022-10-17 18:08:11 +0900

    feat: QueryDSL 설정파일 추가 및 주석 추가

--- src/main/java/com/deca/SpringJpaPractice/domain/order/repository/OrderRepository.java
+++ src/main/java/com/deca/SpringJpaPractice/domain/order/repository/OrderRepository.java
@@ -5,7 +5,7 @@ import com.deca.SpringJpaPractice.domain.order.entity.Order;
 import com.deca.SpringJpaPractice.domain.order.entity.OrderStatus;
 import com.deca.SpringJpaPractice.domain.order.entity.QOrder;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -21,6 +21,8 @@ import static com.deca.SpringJpaPractice.domain.order.entity.QOrder.order;
 public class OrderRepository {
   private final EntityManager em;
 
+  private final JPAQueryFactory jpaQueryFactory;
+
   public void save(Order order) {
     em.persist(order);
   }
@@ -30,20 +32,24 @@ public class OrderRepository {
   }
 
   public List<Order> findAll(OrderSearch orderSearch) {
-    // order와 order와 연관된 member를 join
-    // 검색 조건에 따라서 동적인 쿼리가 필요
-    // ex) 조건에 이름이 ??인 or 모든 이름 + 주문 상태가 NULL, ORDER, CANCEL 인지
+
     QOrder order = QOrder.order;
     QMember member = QMember.member;
-    JPAQuery<Order> query = new JPAQuery<>(em);
 
-    return query
-            .select(order)
-            .from(order)
+    // 검색 조건에 따라서 동적인 쿼리가 필요
+    // ex) 이름 = ??, 주문상태 = ?? 등으로 필터링 가능
+    return jpaQueryFactory
+            // order 테이블에서(select(order).from(order)와 동일)
+            .selectFrom(order).
+            // order와 order와 연관된 member를 join
             .join(order.member, member)
+            // 검색 조건의 주문상태가 order의 주문상태와 같은가?
             .where(statusEq(orderSearch.getOrderStatus()),
+                    // 검색 조건의 이름이 회원의 이름과 같은가?
                     nameLike(orderSearch.getMemberName()))
+            // 검색결과 1000개로 제한
             .limit(1000)
+            // 결과 조회
             .fetch();
   }
 
