96d2795a4946a26547075eff930d1114407d006f
JeongHunHui <deca3248@gmail.com>
2022-10-17 15:57:10 +0900

    feat: 주문 검색기능 테스트코드 작성 및 테스트 완료

--- src/test/java/com/deca/SpringJpaPractice/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/deca/SpringJpaPractice/domain/order/service/OrderServiceTest.java
@@ -8,6 +8,7 @@ import com.deca.SpringJpaPractice.domain.member.entity.Member;
 import com.deca.SpringJpaPractice.domain.order.entity.Order;
 import com.deca.SpringJpaPractice.domain.order.entity.OrderStatus;
 import com.deca.SpringJpaPractice.domain.order.repository.OrderRepository;
+import com.deca.SpringJpaPractice.domain.order.repository.OrderSearch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,7 +32,7 @@ public class OrderServiceTest {
   @Test
   public void 상품주문() throws Exception {
     // given
-    Member member = createMember();
+    Member member = createMember("회원1");
 
     Book book = createBook("시골 JPA", 10000, 10);
 
@@ -48,7 +51,7 @@ public class OrderServiceTest {
   @Test(expected = NotEnoughStockException.class)
   public void 상품주문_재고수량초과() throws Exception {
     // given
-    Member member = createMember();
+    Member member = createMember("회원1");
     Item item = createBook("시골 JPA", 10000, 10);
     int orderCount = 11;
 
@@ -62,7 +65,7 @@ public class OrderServiceTest {
   @Test
   public void 주문취소() throws Exception {
     // given
-    Member member = createMember();
+    Member member = createMember("회원1");
     Book item = createBook("시골 JPA", 10000, 10);
     int orderCount = 2;
     Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
@@ -75,9 +78,55 @@ public class OrderServiceTest {
     assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
   }
 
-  private Member createMember() {
+  @Test
+  public void 주문검색() throws Exception {
+    // given
+    Member member = createMember("회원1");
+    Member member2 = createMember("회원2");
+    Book book = createBook("시골 JPA", 5000, 10);
+    Book book2 = createBook("도시 JPA", 10000, 20);
+    Long orderId = orderService.order(member.getId(), book.getId(), 2);
+    Long orderId2 = orderService.order(member.getId(), book2.getId(), 3);
+    Long orderId3 = orderService.order(member2.getId(), book.getId(), 4);
+    orderRepository.findOne(orderId).cancel();
+    Order order = orderRepository.findOne(orderId);
+    Order order2 = orderRepository.findOne(orderId2);
+    Order order3 = orderRepository.findOne(orderId3);
+
+    // when
+    // 주문자가 "회원1"인 주문 검색
+    OrderSearch search = new OrderSearch();
+    search.setMemberName("회원1");
+    List<Order> orders = orderRepository.findAll(search);
+    // 전체 검색
+    OrderSearch search2 = new OrderSearch();
+    List<Order> orders2 = orderRepository.findAll(search2);
+    // 상태가 "ORDER"인 주문 검색
+    OrderSearch search3 = new OrderSearch();
+    search3.setOrderStatus(OrderStatus.ORDER);
+    List<Order> orders3 = orderRepository.findAll(search3);
+
+    // then
+    // 검색결과 1(회원1)
+    assertEquals("결과1의 결과 수는 2이다.", 2, orders.size());
+    assertEquals("결과1에 주문1이 포함되어 있어야 한다.", true, orders.contains(order));
+    assertEquals("결과1에 주문2가 있어야 한다.", true, orders.contains(order2));
+    assertEquals("결과1에 주문3이 포함되어 있으면 안된다.", false, orders.contains(order3));
+    // 검색결과 2(전체)
+    assertEquals("결과2의 결과 수는 2이다.", 3, orders2.size());
+    assertEquals("결과2에 주문1이 포함되어 있어야 한다.", true, orders2.contains(order));
+    assertEquals("결과2에 주문2가 포함되어 있어야 한다.", true, orders2.contains(order2));
+    assertEquals("결과2에 주문3이 포함되어 있어야 한다.", true, orders2.contains(order3));
+    // 검색결과 3(주문상태 ORDER)
+    assertEquals("결과3의 결과 수는 2이다.", 2, orders3.size());
+    assertEquals("결과3에 주문1이 포함되어 있으면 안된다.", false, orders3.contains(order));
+    assertEquals("결과3에 주문2가 포함되어 있어야 한다.", true, orders3.contains(order2));
+    assertEquals("결과3에 주문3이 포함되어 있어야 한다.", true, orders3.contains(order3));
+  }
+
+  private Member createMember(String name) {
     Member member = new Member();
-    member.setName("회원1");
+    member.setName(name);
     member.setAddress(new Address("서울", "강가", "123-123"));
     em.persist(member);
     return member;
